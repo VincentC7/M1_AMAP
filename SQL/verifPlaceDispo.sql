1c6ca0c95e788e529db9105b4a6660de5acadfee
PIERRATCharly <31919291+PIERRATCharly@users.noreply.github.com>
2021-01-19 06:50:12 +0100

    Correction verifPlaceDispo

--- SQL/verifPlaceDispo.sql
+++ SQL/verifPlaceDispo.sql
@@ -4,12 +4,18 @@ IF ((SELECT nbAbonnementMax FROM Parametre)
 	-
     (SELECT COUNT(*) FROM Abonnement WHERE etat = 'Validé' AND trimestre = trimestre)) <= 0
 	THEN
-     		new.etat := 'Sur liste d’attente';
+     		IF CURRENT_DATE >= (SELECT dateDebut FROM Trimestre WHERE id_trimestre = new.trimestre)
+     	      	AND CURRENT_DATE <= (SELECT dateFin FROM Trimestre WHERE id_trimestre = new.trimestre)
+     			THEN
+				new.etat := 'Validé';
+				UPDATE Utilisateur set role ='Abonné' WHERE id_utilisateur = new.utilisateur;
+				new.datedebutabo := CURRENT_DATE;
+				new.datepaiement := CURRENT_DATE;
+     			ELSE
+     				new.etat := 'Sur liste d’attente';
+     			END IF;
 	ELSE
-     		new.etat := 'Validé';
-		UPDATE Utilisateur set role ='Abonné' WHERE id_utilisateur = new.utilisateur;
-		new.datedebutabo := CURRENT_DATE;
-		new.datepaiement := CURRENT_DATE;
+     		new.etat := 'Sur liste d’attente';	
 	END IF;
 	RETURN NEW;
  END;
